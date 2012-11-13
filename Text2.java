278c511fb767bff71b43763c05ac16e34ad325d4
libohan <libohan01@gmail.com>
2012-11-13 19:27:30 +0800

    sss
    sss

---
+++ Text2.java
@@ -0,0 +1,10 @@
+//package com.cat;
+
+public class Text2 extends T1 extands AbstractText{
+	public  void g(){};
+	public  int animal(){};
+	public static void main(String args[]){
+		System.out.println("gggggg");
+	}
+}
+	
